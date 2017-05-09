6788c39e8a57a186e726cee2f3a37f05468b05a7
Jose L. Louis <jllouis@live.com>
2017-05-09 13:35:34 -0400

    Update Popular_Stations.py: Fixed bugs, and made ability to different stations easier. \n Updated Latest_Turnstile_Counts_At_Midnight.csv: Added Header.

--- Popular_Stations.py
+++ Popular_Stations.py
@@ -2,18 +2,21 @@ import csv
 import operator
 
 d = dict()
+numStations = 0
 
-with open('Latest_Turnstile_Counts.csv', 'rb') as fi:
+with open('Latest_Turnstile_Counts_At_Midnight.csv', 'rb') as fi:
     reader = csv.DictReader(fi)
     for row in reader:
         if row['STATION'] not in d:
-            d[row['STATION']] = [int(row['ENTRIES']), int(row['EXITS'])]
+            d[row['STATION']] = [int(row['ENTRIES']), int(row['EXITS']), str(row['LINENAME'])]
+            numStations += 1
         else:
             d[row['STATION']][0] += int(row['ENTRIES'])
             d[row['STATION']][1] += int(row['EXITS'])
 
-top_entry_stations = sorted(d.items(), key=operator.itemgetter(1))
-top_exit_stations = sorted(d.items(), key=operator.itemgetter(2))
+top_entry_stations = sorted(d.items(), key=operator.itemgetter(0), reverse=True)
+top_exit_stations = sorted(d.items(), key=operator.itemgetter(1), reverse=True)
+print "Number of Stations: " + str(numStations)
 
 # print top 50 stations
 print "Top Exit Stations:"
