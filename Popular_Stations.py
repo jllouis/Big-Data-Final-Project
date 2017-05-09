4dcbb56d9f80b21dfdcf80be01122a9374d2fe20
Jose L. Louis <jllouis@users.noreply.github.com>
2017-05-09 11:30:57 -0400

    Update Popular_Stations.py
    Added code to calculate both popular entry and popular exit stations and print them out. Bug still exists where the exit station quantity does not get updated

--- Popular_Stations.py
+++ Popular_Stations.py
@@ -12,10 +12,16 @@ with open('Latest_Turnstile_Counts.csv', 'rb') as fi:
             d[row['STATION']][0] += int(row['ENTRIES'])
             d[row['STATION']][0] += int(row['EXITS'])
 
-# top_stations = sorted(d.items(), key=operator.itemgetter(1))
-#
-# # print top 50 stations
-# print "printing top stations:"
-# for x in range(10):
-#         print top_stations[x]
+top_entry_stations = sorted(d.items(), key=operator.itemgetter(1))
+top_exit_stations = sorted(d.items(), key=operator.itemgetter(2))
+
+# print top 50 stations
+print "Top Exit Stations:"
+for x in range(50):
+        print top_exit_stations[x]
+
+print "Top Entry Stations: "
+for x in range(50):
+    print top_entry_stations[x]
+
 
