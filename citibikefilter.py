62601ce749657fa260322b5b577192a6a3bf32de
ginadi011 <dicarlo.g.m@gmail.com>
2017-05-08 20:50:54 -0400

    Create citibikefilter.py
    Filtering python script for citibike data set, returns only necessary columns (tripduration,long,lat,star time)

---
+++ citibikefilter.py
@@ -0,0 +1,18 @@
+def csvRows(filename):
+   with open(filename, 'r') as fi:
+       reader = csv.DictReader(fi)
+       for row in reader:
+           yield row
+
+trip_info = []            
+for row in csvRows('citibike.csv'):
+   dates, time = row['starttime'].split(' ')
+   hours,minutes,seconds = time.split(':')
+   if int(hours) >= 7 and int(hours) <= 9 :
+           tripduration = row['tripduration']
+           longitude = row['start_station_longitude']
+           latitude = row['start_station_latitude']
+           starttime = row['starttime']
+           trip_info.append((tripduration, longitude,latitude,starttime))
+
+print trip_info
