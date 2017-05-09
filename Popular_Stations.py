4fab24cf418c3de9197db8bbd54757ed751755c0
Jose L. Louis <jllouis@live.com>
2017-05-09 12:17:50 -0400

    Fixed 'exit not incrementing' bug

--- Popular_Stations.py
+++ Popular_Stations.py
@@ -10,7 +10,7 @@ with open('Latest_Turnstile_Counts.csv', 'rb') as fi:
             d[row['STATION']] = [int(row['ENTRIES']), int(row['EXITS'])]
         else:
             d[row['STATION']][0] += int(row['ENTRIES'])
-            d[row['STATION']][0] += int(row['EXITS'])
+            d[row['STATION']][1] += int(row['EXITS'])
 
 top_entry_stations = sorted(d.items(), key=operator.itemgetter(1))
 top_exit_stations = sorted(d.items(), key=operator.itemgetter(2))
