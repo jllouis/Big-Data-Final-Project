b67a94bb8571c121e5acba3d2673fb6816f3faba
ginadi011 <dicarlo.g.m@gmail.com>
2017-05-10 19:17:52 -0400

    Update CitibikeFilterPt2.py

--- CitibikeFilterPt2.py
+++ CitibikeFilterPt2.py
@@ -3,16 +3,17 @@ import sys
 
 trip_info = []
 for row in sys.stdin:
-   if row[0] != "cartodb_id":
-       row = row.split(',')
-       dates,time = row[3].split(' ')
-       hours,minutes,seconds = time.split(':')
-       if int(hours) >= 7 and int(hours) <= 9:
-            tripduration = row[2]
-            longitude = row[7]
-            latitude = row[6]
-            starttime = row[3]
-            trip_info.append((tripduration, longitude,latitude,starttime))
-            sys.stdout.write(''.join(str(a) for a in trip_info))
+   if row[0] == "cartodb_id":
+       row(next)
+   row = row.split(',')
+   dates,time = row[3].split(' ')
+   hours,minutes,seconds = time.split(':')
+   if int(hours) >= 7 and int(hours) <= 9:
+        tripduration = row[2]
+        longitude = row[7]
+        latitude = row[6]
+        starttime = row[3]
+        trip_info.append((tripduration, longitude,latitude,starttime))
+        sys.stdout.write(''.join(str(a) for a in trip_info))
 
 sys.stdout.flush()
