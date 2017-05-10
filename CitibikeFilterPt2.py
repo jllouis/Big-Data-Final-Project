23797a3692394f36e7dd92a4f182efe875ba80c3
ginadi011 <dicarlo.g.m@gmail.com>
2017-05-10 12:52:50 -0400

    Update CitibikeFilterPt2.py

--- CitibikeFilterPt2.py
+++ CitibikeFilterPt2.py
@@ -5,10 +5,3 @@ for row in sys.stdin:
    # dates,time = row[3].split(' ')
    # hours,minutes,seconds = time.split(':')
    sys.stdout.write(row[3])
-   if int(hours) >= 7 and int(hours) <= 9:
-        tripduration = row[2]
-        longitude = row[7]
-        latitude = row[6]
-        starttime = row[3]
-        trip_info.append((tripduration, longitude,latitude,starttime))
-        sys.stdout.write(trip_info)
