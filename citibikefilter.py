f8e29d5d9e78351778754fea27ad9049ac587879
ginadi011 <dicarlo.g.m@gmail.com>
2017-05-10 13:24:11 -0400

    Update citibikefilter.py

--- citibikefilter.py
+++ citibikefilter.py
@@ -5,22 +5,23 @@ import sys
 
 citbike_files = 'user/gdicarl000/projectdata/citibike'
 
-for file in citbike_files:
-    def csvRows(filename):
-        with open(filename, 'r') as fi:
-            reader = csv.DictReader(fi)
-            for row in reader:
-                yield row
+with open(citibike_files, 'r') as citibike_files:
+    for file in citbike_files:
+        def csvRows(filename):
+            with open(filename, 'r') as fi:
+                reader = csv.DictReader(fi)
+                for row in reader:
+                    yield row
     
-trip_info = []            
-for row in csvRows(file):
-    dates, time = row['starttime'].split(' ')
-    hours,minutes,seconds = time.split(':')
-    if int(hours) >= 7 and int(hours) <= 9 :
-        tripduration = row['tripduration']
-        longitude = row['start_station_longitude']
-        latitude = row['start_station_latitude']
-        starttime = row['starttime']
-        trip_info.append((tripduration, longitude,latitude,starttime))
+    trip_info = []            
+    for row in csvRows(file):
+        dates, time = row['starttime'].split(' ')
+        hours,minutes,seconds = time.split(':')
+        if int(hours) >= 7 and int(hours) <= 9 :
+            tripduration = row['tripduration']
+            longitude = row['start_station_longitude']
+            latitude = row['start_station_latitude']
+            starttime = row['starttime']
+            trip_info.append((tripduration, longitude,latitude,starttime))
 
 print trip_info
