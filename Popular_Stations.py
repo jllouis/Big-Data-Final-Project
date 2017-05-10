8d667d30aec8f0dca3c4a5d9ef3e47ab3fd262db
Jose L. Louis <jllouis@live.com>
2017-05-10 19:12:38 -0400

    Added the output for the popular subway stations that were able to be matched. And updated semi-working Popular_Stations.py

--- Popular_Stations.py
+++ Popular_Stations.py
@@ -1,1 +1,52 @@
-import csvimport operatorstation_totals = dict()numStations = 0unmatched = 0with open('Latest_Turnstile_Counts_At_Midnight.csv', 'rb') as fi:    reader = csv.DictReader(fi)    for row in reader:        if row['STATION'] not in station_totals:            station_totals[str(row['STATION'])] = [int(row['ENTRIES']), int(row['EXITS']),                                                   str(row['LINENAME']), None]            numStations += 1        else:            station_totals[row['STATION']][0] += int(row['ENTRIES'])            station_totals[row['STATION']][1] += int(row['EXITS'])station_locations = dict()with open('DOITT_SUBWAY_STATION_01_13SEPT2010.csv') as fi:    reader = csv.DictReader(fi)    for record in reader:        station_locations[str(record['NAME']).upper()] = record['the_geom']    for station in station_locations:        station = str(station).replace('TH', '')        station = str(station).replace('ND', '')for station in station_totals:    if station in station_locations:        station_totals[station] = [station_totals[station][0], station_totals[station][1],                                   station_totals[station][2], station_locations[station]]    else:        unmatched += 1print "Was unable to match " + str(unmatched) + " stations."top_entry_stations = sorted(station_totals.items(), key=operator.itemgetter(0), reverse=True)top_exit_stations = sorted(station_totals.items(), key=operator.itemgetter(1), reverse=True)print "Number of Stations: " + str(numStations)# print top 50 stationsprint "Top Exit Stations:"for x in range(50):        print top_exit_stations[x]print "Top Entry Stations: "for x in range(50):    print top_entry_stations[x]
+import csv
+import operator
+
+station_totals = dict()
+numStations = 0
+unmatched = 0
+
+with open('Latest_Turnstile_Counts_At_Midnight.csv', 'rb') as fi:
+    reader = csv.DictReader(fi)
+    for row in reader:
+        if row['STATION'] not in station_totals:
+            station_totals[str(row['STATION'])] = [int(row['ENTRIES']), int(row['EXITS']),
+                                                   str(row['LINENAME']), None]
+            numStations += 1
+        else:
+            station_totals[row['STATION']][0] += int(row['ENTRIES'])
+            station_totals[row['STATION']][1] += int(row['EXITS'])
+
+station_locations = dict()
+
+with open('DOITT_SUBWAY_STATION_01_13SEPT2010.csv') as fi:
+    reader = csv.DictReader(fi)
+    for record in reader:
+        station_locations[str(record['NAME']).upper()] = record['the_geom']
+
+    for station in station_locations:
+        station = str(station).replace('TH', '')
+        station = str(station).replace('ND', '')
+
+for station in station_totals:
+    if station in station_locations:
+        station_totals[station] = [station_totals[station][0], station_totals[station][1],
+                                   station_totals[station][2], station_locations[station]]
+    else:
+        unmatched += 1
+
+print "Was unable to match " + str(unmatched) + " stations."
+
+top_entry_stations = sorted(station_totals.items(), key=operator.itemgetter(0), reverse=True)
+top_exit_stations = sorted(station_totals.items(), key=operator.itemgetter(1), reverse=True)
+
+
+print "Number of Stations: " + str(numStations)
+
+# print top 50 stations
+print "Top Exit Stations:"
+for x in range(50):
+        print top_exit_stations[x]
+
+print "Top Entry Stations: "
+for x in range(50):
+    print top_entry_stations[x]
