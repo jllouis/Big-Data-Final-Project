f40f3059db17a7cdb3b923f55b507c7929043efd
ginadi011 <dicarlo.g.m@gmail.com>
2017-05-10 09:38:04 -0400

    Update citibikefilter.py

--- citibikefilter.py
+++ citibikefilter.py
@@ -12,16 +12,15 @@ with open(citbike_files, 'r') as citibike_file:
          for row in reader:
             yield row
       
-      
-   trip_info = []            
-   for row in csvRows(file):
-      dates, time = row['starttime'].split(' ')
-      hours,minutes,seconds = time.split(':')
-      if int(hours) >= 7 and int(hours) <= 9 :
-      tripduration = row['tripduration']
-      longitude = row['start_station_longitude']
-      latitude = row['start_station_latitude']
-      starttime = row['starttime']
-      trip_info.append((tripduration, longitude,latitude,starttime))
+      trip_info = []            
+      for row in csvRows(file):
+         dates, time = row['starttime'].split(' ')
+         hours,minutes,seconds = time.split(':')
+         if int(hours) >= 7 and int(hours) <= 9 :
+         tripduration = row['tripduration']
+         longitude = row['start_station_longitude']
+         latitude = row['start_station_latitude']
+         starttime = row['starttime']
+         trip_info.append((tripduration, longitude,latitude,starttime))
 
 print trip_info
