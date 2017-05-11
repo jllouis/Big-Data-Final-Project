222c9b056b194735aa627df52a58c7a7b8a88c35
Binod Timalsina <t17711@users.noreply.github.com>
2017-05-10 20:46:05 -0400

    Add files via upload
    created a way for spark to read multiple csv and combine then using dataframe, deeds debuging

---
+++ test_read_multiple_citicsv_by_spark.py
@@ -0,0 +1,36 @@
+from datetime import datetime  
+from datetime import timedelta
+
+def next_month(start):
+    month = start.month
+    while month == start.month:
+        start+=timedelta(days = 1)
+    return start
+
+
+def read_citibike_to_dataframe():
+    end = datetime.strptime('201612', '%Y%m')
+    start = datetime.strptime('201307', '%Y%m')
+    while start <= end:
+        cur = '/user/gdicarl000/projectdata/citibike/'+start.strftime('%Y%m')+'-citibike-tripdata.csv'
+        print cur
+        c = sqlContext.read.format("csv").option("delimiter",",").option("quote","").option("inferSchema", "true").load(cur)
+        start= next_month(start)
+        yield c
+   
+
+#
+
+def get_one_citi():   
+    citi = read_citibike_to_dataframe()
+    s =None
+    for i in citi:
+        if s == None:
+            s = i
+        else:
+            s.unionAll(i)
+    return s
+
+c =get_one_citi()
+c.registerTempTable('test')
+c.write.format('com.databricks.spark.csv').save('/user/gdicarl000/projectdata/citibike/temp_spark_combine')
