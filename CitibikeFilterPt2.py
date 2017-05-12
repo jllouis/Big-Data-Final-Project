2571a357abd0477d6f2ade31cef3da8231b6f693
ginadi011 <dicarlo.g.m@gmail.com>
2017-05-12 17:53:36 -0400

    Update CitibikeFilterPt2.py

--- CitibikeFilterPt2.py
+++ CitibikeFilterPt2.py
@@ -10,7 +10,7 @@ for row in sys.stdin:
         dates,time = row[1].split(' ')
         hours,minutes,seconds = time.split(':')
         if int(hours) >= 7 and int(hours) <= 9:
-             sys.stdout.write(row[0] +',')
+             sys.stdout.write(row[5] +',')
              sys.stdout.write(row[6]+',')
              sys.stdout.write(row[7]+',')
              sys.stdout.write(row[9]+',')
