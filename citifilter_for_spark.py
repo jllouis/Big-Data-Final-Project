d2a3f3ec62cbd5fe18829f22064e55a689f2b4dd
Binod Timalsina <t17711@users.noreply.github.com>
2017-05-12 23:38:11 +0000

    Update citifilter_for_spark.py

--- citifilter_for_spark.py
+++ citifilter_for_spark.py
@@ -8,23 +8,4 @@ def parseCITIBIKECSV(idx, part):
         dates,time = row[1].split(' ')
         hours,minutes,seconds = time.split(':')
         if int(hours) >= 7 and int(hours) <= 9:
-         yield 
-
-count = 0
-for row in sys.stdin:
-   if count == 0:
-        count = 1
-   else:
-        row = row.split(',')
-        dates,time = row[1].split(' ')
-        hours,minutes,seconds = time.split(':')
-        if int(hours) >= 7 and int(hours) <= 9:
-             sys.stdout.write(row[5] +',')
-             sys.stdout.write(row[6]+',')
-             sys.stdout.write(row[7]+',')
-             sys.stdout.write(row[9]+',')
-             sys.stdout.write(row[10]+',')
-             sys.stdout.write(row[1]+',')
-
-
-sys.stdout.flush()
+            yield (row[5],row[6],row[7],row[9],row[10],row[1])
