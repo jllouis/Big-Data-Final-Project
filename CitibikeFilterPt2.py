7196548242e2f7b1f01a5423183438ae7d8b612e
ginadi011 <dicarlo.g.m@gmail.com>
2017-05-12 16:14:38 -0400

    Update CitibikeFilterPt2.py

--- CitibikeFilterPt2.py
+++ CitibikeFilterPt2.py
@@ -4,20 +4,20 @@ import sys
 trip_info = []
 count = 0
 for row in sys.stdin:
-   if count == 0:
-       next(row)
-       count = 1
-   row = row.split(',')
-   dates,time = row[1].split(' ')
-   hours,minutes,seconds = time.split(':')
-   if int(hours) >= 7 and int(hours) <= 9:
-        tripduration = row[0]
-        startlongitude = row[5]
-        startlatitude = row[6]
-        stoplatitude = row[8]
-        stoplongitude =row[9]
-        starttime = row[1]
-        trip_info.append((tripduration, startlongitude,startlatitude,stoplatitude,stoplongitude,starttime))
-        sys.stdout.write(''.join(str(a) for a in trip_info))
+   if count = 0:
+        count = 1
+   else:
+        row = row.split(',')
+        dates,time = row[1].split(' ')
+        hours,minutes,seconds = time.split(':')
+        if int(hours) >= 7 and int(hours) <= 9:
+             tripduration = row[0]
+             startlongitude = row[5]
+             startlatitude = row[6]
+             stoplatitude = row[8]
+             stoplongitude =row[9]
+             starttime = row[1]
+             trip_info.append((tripduration, startlongitude,startlatitude,stoplatitude,stoplongitude,starttime))
+             sys.stdout.write(''.join(str(a) for a in trip_info))
 
 sys.stdout.flush()
