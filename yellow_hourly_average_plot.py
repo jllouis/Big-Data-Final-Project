eb00df97b3ca28d7cfd9d9938bf605ee22d6a6e6
Binod Timalsina <t17711@users.noreply.github.com>
2017-05-13 23:38:02 +0000

    modified to get avg mile by hour, need to test

--- yellow_hourly_average_plot.py
+++ yellow_hourly_average_plot.py
@@ -13,22 +13,25 @@ import pandas as pd
 plt.switch_backend('agg') # to enable plot in backend
 
 
-'''0) tripduration,   => 634
-   1) starttime,      => 2013-07-01 00:00:00  datetime.strptime('2013-07-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-   2) stoptime,       => 2013-07-01 00:10:34
-   3) start station id, => 164
-   4) start station name, => E 47 St & 2 Ave
-   5) start station latitude, => 40.75323098
-   6) start station longitude, => -73.97032517
-   7) end station id,   => 504
-   8) end station name,  => 1 Ave & E 15 St
-   9) end station latitude,=> 40.73221853
-   10) end station longitude, => -73.98165557
-   11) bikeid,        => 16950
-   12) usertype,      => Customer
-   13) birth year,    => \N
-   14) gender,        => 0
-
+'''
+0 VendorID,
+1 tpep_pickup_datetime
+2 tpep_dropoff_datetime
+3 passenger_count
+4 trip_distance
+5 pickup_longitude
+6 pickup_latitude
+7 RateCodeID
+8 store_and_fwd_flag
+9 dropoff_longitude
+10 dropoff_latitude
+11 payment_type
+12 fare_amount
+13 extra
+14 mta_tax
+15 tip_amount
+16 tolls_amount
+17 total_amount
 '''
 
 # get next month
@@ -39,15 +42,16 @@ def next_month(start):
    return start
 
 #parse citi csv
-def parseCITIBIKECSV(idx, part):
+def parseYELLOWCSV(idx, part):
    if(idx==0):
       part.next()
    for line in part:
       row = line.split(',')
-      date = row[1][1:-1].split(' ')
+      date = row[1].split(' ')
       hour = date[1].split(':')[0]
       #if hour >= 7 and hour <= 9:
-      yield (float(row[5][1:-1]),float(row[6][1:-1]),int(row[7][1:-1]),float(row[9][1:-1]),float(row[10][1:-1]),int(hour))
+      yield (
+      yield (float(row[6]),float(row[5]),int(row[0]),float(row[10]),float(row[9]),int(hour))
          
 def get_miles(part):
    start = (part[0], part[1])
@@ -56,108 +60,47 @@ def get_miles(part):
    return (part[5], m)
    
 
-# create rdd, read from citibike files
-# map rdd to a schema we want
-# define that schema
-# convert rdd to rdd using that scmema
-def read_citibike_to_dataframe():                      
-   end = datetime.strptime('201703', '%Y%m')
-   start = datetime.strptime('201307', '%Y%m')
+# create rdd, read from yellow files
+#convert to data frame
+def read_yellow_to_dataframe():                      
+
    # create fields to give csv structure
-   field_name = ['start_latitude','start_longitude', 'end_id', 'end_latitude', 'end_longitude','starttime']   
+   field_name = ['start_latitude','start_longitude', 'vendor_id', 'end_latitude', 'end_longitude','starttime']   
    field_type = [FloatType(), FloatType(),IntegerType(), FloatType(), FloatType(), StringType()]
+  
    # create schema
    field=[]
    for i in range(0,6):
       print i
       field.append(StructField(field_name[i], field_type[i]))
-   schema = StructType(field)
-   while start < end:
-      cur = '/user/gdicarl000/projectdata/citibike/'+start.strftime('%Y%m')+'-citibike-tripdata.csv'
-      c2 = sc.textFile(cur).mapPartitionsWithIndex(parseCITIBIKECSV)
-      df = sqlContext.createDataFrame(c2,schema)
-      start= next_month(start)
-      #df.show(10)
-      print cur
-      #print c2.count()
-      yield df
-        
+   schema = StructType(field)  
+   cur = '/user/gdicarl000/projectdata/cardata.csv'
+   c2 = sc.textFile(cur).mapPartitionsWithIndex(parseCITIBIKECSV)
+   df = sqlContext.createDataFrame(c2,schema)
+   return df
 
-# for each citibike there is df, union them all
+def save_dataframe_to_plot(df):
+   # for miles
+   avgRDD = yellow.rdd.map(get_miles)
+   avgRDD= avgRDD.mapValues(lambda x: (x,1))
+   avgRDD = avgRDD.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+   avgRDD= avgRDD.mapValues(lambda y : 1.0 * y[0] / y[1])
+   #avgRDD.show(2)
 
-def get_one_citi():
-   citi = read_citibike_to_dataframe()
-   s =None
-   for i in citi:
-      if s == None:
-         print('start')
-         s = i
-      else:
-         if i!=None:
-            s = s.unionAll(i)
-   return s
+   schema2 = StructType([StructField("Hour", IntegerType()), StructField("Miles", FloatType())])
+   mdf = sqlContext.createDataFrame(avgRDD,schema2)
+   #mdf.show(10)
+
+   # convert to panda df
+   mpdDF = mdf.toPandas()
+   return mpDF
 
 # get combined dataframe of all
-comb =get_one_citi()
-#comb.createOrReplaceTempView("citibike")
-#results = spark.sql("SELECT count(*) FROM citibike")
-#print(results.show())
+yellow =read_yellow_to_dataframe()
+yellowMpdDF = save_dataframe_to_plot(yellow)
 
-#store table
-#comb.coalesce(1).write.format('com.databricks.spark.csv').option("header", "true").save('/user/btimals000/citibike_spark1.csv')
-
-# for miles
-avgRDD = comb.rdd.map(get_miles)
-avgRDD= avgRDD.mapValues(lambda x: (x,1))
-avgRDD = avgRDD.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-avgRDD= avgRDD.mapValues(lambda y : 1.0 * y[0] / y[1])
-#avgRDD.take(2)
-
-schema2 = StructType([StructField("Hour", IntegerType()), StructField("Miles", FloatType())])
-mdf = sqlContext.createDataFrame(avgRDD,schema2)
-#mdf.show(10)
-
-# convert to pdf
-mpdDF = mdf.toPandas()#pdf = pdf.cumsum()
-#mpdDF['Time'] = pd.to_datetime(mpdDF.Epoc_Time,unit='s')
-
-mpdDF.plot(x='Hour', y='Miles', kind='scatter')
-plt.savefig('miles_avg.png')
+YellowsDF=yellowMpdDF.sort_values('Hour',  ascending=False)         
+YellowsDF.plot(x='Hour', y='Miles',linestyle='--', marker='o', color='r', kind='line',grid=True)
+plt.savefig("yellow_by_hour.png")     
 
 
-###########################
-#for single file for  testing
-# create fields to give csv structure
-'''
-field_name = ['start_latitude','start_longitude', 'end_id', 'end_latitude', 'end_longitude','starttime']   
-field_type = [FloatType(), FloatType(),IntegerType(), FloatType(), FloatType(), IntegerType()]
-# create schema
-field=[]
-for i in range(0,6):
-   print i
-   field.append(StructField(field_name[i], field_type[i]))
-        
-schema = StructType(field)
-cur = '/user/gdicarl000/projectdata/citibike/201307-citibike-tripdata.csv'
-c_single = sc.textFile(cur).mapPartitionsWithIndex(parseCITIBIKECSV)
-df = sqlContext.createDataFrame(c_single,schema)
-df.show(10)
-mdf = df.mapPartitionsWithIndex(get_miles)
-df.createOrReplaceTempView("citibike")
-results = spark.sql("SELECT * FROM citibike")
-print(results.show())
-
-
-
-###########
-#print single for test
-
-# convert to pdf
-#pdDF = df.toPandas()#pdf = pdf.cumsum()
-#pdDF.plot(x='starttime', y='start_latitude')
-#plt.savefig('foo.png')
-
-cur = '/user/gdicarl000/projectdata/citibike/201307-citibike-tripdata.csv'
-c_single = sc.textFile(cur).mapPartitionsWithIndex(parseCITIBIKECSV)
-df = sqlContext.createDataFrame(c_single,schema)
-'''
